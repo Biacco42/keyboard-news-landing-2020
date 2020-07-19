b95d8bc6507293ed04d9c57ec37141002abffbf2
Biacco42 <biacco42@gmail.com>
2020-07-19 16:59:29 +0900

    Perevent mobile to load YouTubeAPI

--- scripts/kbdnews.js
+++ scripts/kbdnews.js
@@ -1,9 +1,11 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 let mobileWidth = 768;
+
+if (mobileWidth < window.innerWidth) {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
 
 function onYouTubeIframeAPIReady() {
     ytPlayer = new YT.Player(
@@ -29,16 +31,12 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
     event.target.mute();
-    if (mobileWidth < window.innerWidth) {
-        event.target.playVideo();
-    }
+    event.target.playVideo();
 }
 
 function onPlayerStateChange(event) {
     var ytStatus = event.target.getPlayerState();
-    if (ytStatus == YT.PlayerState.ENDED && mobileWidth < window.innerWidth) {
-        event.target.mute();
-        event.target.playVideo();
-    }
+    event.target.mute();
+    event.target.playVideo();
 }
 
