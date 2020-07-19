7cb5ad08b79e8ccbfd2c038b40b930ff5e44bf9c
Biacco42 <biacco42@gmail.com>
2020-07-19 16:15:46 +0900

    Add header

---
+++ scripts/kbdnews.js
@@ -0,0 +1,40 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+    ytPlayer = new YT.Player(
+        'header-video',
+        {
+            videoId: 'DU0ZbbAaXn0',
+            playerVars: {
+                loop: 1,
+                controls: 0,
+                autoplay: 1,
+                disablekb: 1,
+                rel: 0,
+                showinfo: 0,
+                enablesjsapi: 1
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        }
+    );
+}
+
+function onPlayerReady(event) {
+    event.target.mute();
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    var ytStatus = event.target.getPlayerState();
+    if (ytStatus == YT.PlayerState.ENDED) {
+        event.target.mute();
+        event.target.playVideo();
+    }
+}
+
